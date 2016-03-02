ee411ea0131bf35f7817b5c42900b2e60e6fb04c
Mauricio Garavaglia <mauricio@medallia.com>
2016-03-02 00:13:46 -0300

    update entrypoint

--- scheduler.sh
+++ scheduler.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 AURORA_HOME=/usr/local/aurora-scheduler
 EXECUTOR_HOME=/opt/aurora/executors
+AURORA_DATA=/opt/aurora
 
 # Flags controlling the JVM.
 JAVA_OPTS=(
@@ -11,26 +12,30 @@ JAVA_OPTS=(
 )
 
 # Flags controlling the scheduler.
+# AURORA_FLAGS=(
+#   -cluster_name=$CLUSTER
+#   -native_log_quorum_size=$NATIVE_LOG_QUORUM_SIZE
+#   -zk_endpoints=$ZK
+#   -mesos_master_address=$MESOS_ZK 
+#   -thermos_executor_resources=$EXECUTOR_RESOURCES 
+# )
 AURORA_FLAGS=(
-  -cluster_name=$CLUSTER
   -http_port=8081 
-  -native_log_quorum_size=$NATIVE_LOG_QUORUM_SIZE
-  -zk_endpoints=$ZK
-  -mesos_master_address=$MESOS_ZK 
   -serverset_path=/aurora/scheduler 
   -native_log_zk_group_path=/aurora/replicated-log 
   -native_log_file_path=$AURORA_DATA/scheduler/db 
   -backup_dir=$AURORA_DATA/scheduler/backups
   -thermos_executor_path=$EXECUTOR_HOME/thermos_executor.pex
-  -thermos_executor_resources=$EXECUTOR_RESOURCES 
   -allow_docker_parameters=true
   -vlog=INFO 
   -logtostderr 
   -allowed_container_types=MESOS,DOCKER 
 )
 
+ARGUMENTS=( "${AURORA_FLAGS[@]}" "$@" )
+
 # Environment variables controlling libmesos
 export GLOG_v=1
 export LIBPROCESS_PORT=8083
 
-JAVA_OPTS="${JAVA_OPTS[*]}" exec "$AURORA_HOME/bin/aurora-scheduler" "${AURORA_FLAGS[@]}"
+JAVA_OPTS="${JAVA_OPTS[*]}" exec "$AURORA_HOME/bin/aurora-scheduler" "${ARGUMENTS[@]}"
